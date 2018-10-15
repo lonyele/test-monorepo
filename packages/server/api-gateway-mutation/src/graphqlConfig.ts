7a287aa2b6db3422653597268c1594a875b8334c
Taeheon Kim <skyrabbits1@gmail.com>
2018-10-15 23:56:56 +0900

    api gateway mutation

---
+++ packages/server/api-gateway-mutation/src/graphqlConfig.ts
@@ -0,0 +1,16 @@
+// import { schema } from "./schema";
+// import { initMongo } from "./mongodb/mongo";
+// const mongo = initMongo();
+
+export const GRAPHQL_PORT = 3000;
+// export const apolloExpressConfig = (req: any) => {
+//   return {
+//     schema
+//     // context: {
+//     //   mongo
+//     // }
+//   };
+// };
+// export const graphiqlExpressConfig = {
+//   endpointURL: "./graphql"
+// };
