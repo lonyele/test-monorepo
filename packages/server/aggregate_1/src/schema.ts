97923f728a42d1eca9c36b9cb71dd7aeffb2425a
Taeheon Kim <skyrabbits1@gmail.com>
2018-10-15 23:52:37 +0900

    test aggregate 1

---
+++ packages/server/aggregate_1/src/schema.ts
@@ -0,0 +1,9 @@
+// Put together a schema
+// import { makeExecutableSchema } from "graphql-tools";
+// import { typeDefs } from "./typeDefinitions/root/index";
+// import { resolvers } from "./resolvers/root/index";
+
+// export const schema = makeExecutableSchema({
+//   typeDefs,
+//   resolvers
+// });
